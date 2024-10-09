cc0876dd32bab413f19c770823271c9b5d12cbe6
victortejeda <victorfernandotejeda@gmail.com>
2024-10-09 18:27:17 -0400

    Initial Commit

---
+++ Calculadora/CalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraApp.swift
+//  Calculadora
+//
+//  Created by Victor Tejeda on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
